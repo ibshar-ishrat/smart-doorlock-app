bea63681cc0328660ce9c4d6485f9aae8fd9a66c
Sadia Sami <sadia_sami@ymail.com>
2016-06-28 14:58:13 +0500

    User Registration4 intent calling Door settings layout.

---
+++ app/src/main/java/com/example/sadi/smartdoorapp/User_Registration4.java
@@ -0,0 +1,25 @@
+package com.example.sadi.smartdoorapp;
+
+import android.os.Bundle;
+
+/**
+ * Created by Sami Ullah on 3/16/2016.
+ */
+public class User_Registration4 extends Main_ScreenActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration4);
+     /*   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+}
