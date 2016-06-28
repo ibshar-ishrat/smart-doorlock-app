f70d195ab62bcec10fb09aa9c5df48917d61569a
Sadia Sami <sadia_sami@ymail.com>
2016-06-28 14:17:44 +0500

    functionality added to next button2.

--- app/src/main/java/com/example/sadi/smartdoorapp/User_Registration2.java
+++ app/src/main/java/com/example/sadi/smartdoorapp/User_Registration2.java
@@ -1,11 +1,15 @@
 package com.example.sadi.smartdoorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Sami Ullah on 3/16/2016.
  */
 public class User_Registration2 extends Main_ScreenActivity {
+    private static Button button_next2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,25 @@ public class User_Registration2 extends Main_ScreenActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        ButtonNext2();
     }
+
+
+    public void ButtonNext2()
+    {
+        button_next2=(Button)findViewById(R.id.button_next2);
+        button_next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next2 = new Intent("com.example.sadi.smartdoorapp.activity_registration3");
+                startActivity(next2);
+
+            }
+
+
+
+        });
+    }
+
 }
