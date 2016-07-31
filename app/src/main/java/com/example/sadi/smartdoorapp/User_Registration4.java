44324c767d1e222fa4a50a58f8dcde0cb089d9e0
Sadia Sami <sadia_sami@ymail.com>
2016-07-31 16:58:07 +0500

    Dynamic (Runtime) right side spinner functionality added.

--- app/src/main/java/com/example/sadi/smartdoorapp/User_Registration4.java
+++ app/src/main/java/com/example/sadi/smartdoorapp/User_Registration4.java
@@ -1,9 +1,12 @@
 package com.example.sadi.smartdoorapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Sami Ullah on 3/16/2016.
@@ -15,7 +18,11 @@ public class User_Registration4 extends Main_ScreenActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration4);
         ButtonDone();
+
+
+
     }
+
 
     public void ButtonDone()
     {
@@ -23,6 +30,23 @@ public class User_Registration4 extends Main_ScreenActivity {
         button_done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+               EditText txtItem = (EditText) findViewById(R.id.editText_nameDoor);
+                String text = txtItem.getText().toString();
+                //Intent i = new Intent(User_Registration4.this, sidePanel.class);
+                //i.putExtra("text_label", text);
+
+
+
+                //Here define all your sharedpreferences code with key and value
+                //create object of SharedPreferences
+                SharedPreferences prefs = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
+                //Now get editor
+                SharedPreferences.Editor edit = prefs.edit();
+                //takes value from edit text
+                edit.putString("MID", text );
+                //commits your edit
+                edit.commit();
+
                 Intent next3 = new Intent("com.example.sadi.smartdoorapp.Acknowledgement_Page");
                 startActivity(next3);
 
@@ -32,4 +56,5 @@ public class User_Registration4 extends Main_ScreenActivity {
 
         });
     }
+
 }
--- app/src/main/java/com/example/sadi/smartdoorapp/sidePanel.java
+++ app/src/main/java/com/example/sadi/smartdoorapp/sidePanel.java
@@ -2,11 +2,13 @@ package com.example.sadi.smartdoorapp;
 
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,15 +16,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
+
+import java.util.ArrayList;
 
 public class sidePanel extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /** Items entered by the user is stored in this ArrayList variable */
+    ArrayList<String> list = new ArrayList<String>();
+
+    /** Declaring an ArrayAdapter to set items to ListView */
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side_panel);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -58,6 +72,11 @@ public class sidePanel extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_side_panel_drawer, menu);
+        return true;
+    }*/
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -72,6 +91,22 @@ public class sidePanel extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // flate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.side_panel, menu);
+        /** Getting a reference to Spinner object of the resource activity_main */
+        MenuItem item=menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+        SharedPreferences bb = getSharedPreferences("my_prefs", 0);
+        String m = bb.getString("MID", "");
+        //Intent i = getIntent();
+        //String doorname = i.getStringExtra("text_label");
+        list.add(m);
+
+        /** Defining the ArrayAdapter to set items to Spinner Widget */
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        /** Setting the adapter to the ListView */
+        spinner.setAdapter(adapter);
+        /** Adding radio buttons for the spinner items*/
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         return true;
     }
 
